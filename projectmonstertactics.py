4b919a6a9e667b4ec2283c83c80a77d819464029
Sean Lindell <lindellsean@gmail.com>
2022-02-21 12:07:04 -0500

    initial commit

---
+++ projectmonstertactics.py
@@ -0,0 +1,93 @@
+import pygame
+import spritemanager as sm
+
+def main():
+
+    cursorXPos = 0
+    cursorYPos = 0
+    cursorMovementBuffer = 0
+
+    clock = pygame.time.Clock()
+
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((1280,800))
+
+    # initialize the pygame module
+    pygame.init()
+
+    # load and set the logo
+    logo = pygame.image.load("vampire2.png").convert_alpha()
+    logo = pygame.transform.scale(logo, (64, 64))
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("Project Monster Tactics")
+
+    #Loading tile images, may change later to store them as sprites
+    redTile = pygame.image.load("red_tile.png").convert_alpha()
+    blueTile = pygame.image.load("blue_tile.png").convert_alpha()
+    lblueTile = pygame.image.load("lblue_tile.png").convert_alpha()
+    purpleTile = pygame.image.load("purple_tile.png").convert_alpha()
+    whiteTile = pygame.image.load("white_tile.png").convert_alpha()
+    whiteTile = pygame.transform.scale(whiteTile, (64, 64))
+    lblueTile = pygame.transform.scale(lblueTile, (64, 64))
+    purpleTile = pygame.transform.scale(purpleTile, (64, 64))
+
+    #Creating sprites defined in spritemanager
+    cursor = sm.Cursor()
+    myVamp = sm.Vampire()
+    myWere = sm.Werewolf()
+
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+
+        screen.fill((255, 255, 255))
+
+        cursor.update()
+        myVamp.update()
+        myWere.update()
+
+        for x in range(10):
+            for y in range(10):
+                screen.blit(purpleTile, (100+x*64,100+y*64))
+
+        screen.blit(myVamp.getImage(), (100+1*64,100+1*64))
+        screen.blit(myWere.getImage(), (100+2*64,100+2*64))
+        screen.blit(cursor.getImage(), (100+cursorXPos*64,100+cursorYPos*64))
+
+        pygame.display.flip()
+        clock.tick(30)
+
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+        keystate = pygame.key.get_pressed()
+        if not cursorMovementBuffer:
+            if (keystate[pygame.K_UP] and cursorYPos > 0):
+                cursorYPos -= 1
+                cursorMovementBuffer = 1
+            elif (keystate[pygame.K_DOWN] and cursorYPos < 9):
+                cursorYPos += 1
+                cursorMovementBuffer = 1
+            elif (keystate[pygame.K_RIGHT] and cursorXPos < 9):
+                cursorXPos += 1
+                cursorMovementBuffer = 1
+            elif (keystate[pygame.K_LEFT] and cursorXPos > 0):
+                cursorXPos -= 1
+                cursorMovementBuffer = 1
+        else:
+            cursorMovementBuffer = 0
+        if (keystate[pygame.K_SPACE]):
+            myVamp.animActive = (myVamp.animActive+1)%2
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
---
+++ spritemanager.py
@@ -0,0 +1,121 @@
+import pygame
+import os
+
+# This method is used to load the surfaces needed for animated and non-animated sprites
+# When saving images for sprites, save as pngs and for animated sprites, have each frame
+# be a seperate png differentiated by a number suffix starting at one and counting up.
+# EXAMPLE: Non-animated sprite of cat: file named cat.png    Load it for a sprite with: myCatSprite.images = load_images("cat")
+# *note that you do not include the ".png" in the string with the filename 
+# EXAMPLE: Animated sprite of cat: files named cat1.png, cat2.png, cat3.png, ...    Load it for a sprite with: myCatSprite.images = load_images("cat")
+def load_images(fileName):
+    if(os.path.exists(fileName +".png")):
+        return [pygame.image.load(fileName +".png").convert_alpha()]
+    elif(os.path.exists(fileName +"1.png")):
+        i = 1
+        images = []
+        images.append(pygame.image.load(fileName +"1.png").convert_alpha())
+        while(os.path.exists(fileName + str(i + 1) +".png")):
+            images.append(pygame.image.load(fileName + str(i + 1) +".png").convert_alpha())
+            i += 1
+        return images
+    else:
+        print("Did not find: " + fileName)
+
+# Use instantiations of this class for sprites that don't do much besides moving around.
+# IF YOU WANT A SPRITE WITH MORE FUNCTIONALITY STORED IN IT: create a subclass of this sprite and incorperate that functionality there
+# For example, a cusor. Make sure to set images and animCycle. 
+# They can be set by just passing them as arguments
+# images contains the sprites that make up the animation and should set using the load_images() function, 
+#       or by passing the string for the images as the first argument during instantiation
+# animCycle is how many game-frames each animation-frame should show before changing to the next game. The game runs at
+#       30 game-frames per second, so keep that in mind when deciding how long each frame should last. It is 15 by default
+# If you want the animation to pause, switch the animActive field
+# If you want the animation to skip to a specific frame, use switchToFrame()
+# CORE USEAGE: call mySprite.update() each game-frame, and when blitting to screen, use mySprite.getImage() to get the appopriate frame to draw
+class GenericAnimatedSprite(pygame.sprite.Sprite):
+
+    animCycle = 15
+    animActive = True
+    gameFrameCounter = 0
+    currentSpriteFrame = 0
+    images = []
+
+    def __init__(self, images, animCycle):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = load_images(images)
+        self.animCycle = animCycle
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    def update(self):
+        if self.animActive:
+            self.gameFrameCounter += 1
+            if(self.gameFrameCounter == self.animCycle):
+                self.currentSpriteFrame += 1
+                self.gameFrameCounter = 0
+                if(self.currentSpriteFrame == len(self.images)):
+                    self.currentSpriteFrame = 0
+    
+    def switchToFrame(self, frame):
+        if(frame<len(self.images) and frame > -1):
+            self.currentSpriteFrame = frame
+            self.gameFrameCounter = 0
+        else:
+            print("Frame out of bounds")
+
+    def getImage(self):
+        return self.images[self.currentSpriteFrame]
+
+
+# Basic sprite which is not animated
+class GenericStaticSprite(pygame.sprite.Sprite):
+
+    images = []
+
+    def __init__(self, images):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = images
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+    def getImage(self):
+        return self.images[0]
+
+
+# Subclass of GenericAnimatedSprite intended to be used by creating further subclasses for each unit type.
+# Avoid instatantiating this sprite on its own
+class PlayerUnitSprite(GenericAnimatedSprite):
+
+    maxHP = 1
+    HP = 1
+    ATK = 1
+    MOV = 3
+
+    def __init__(self, maxHP, ATK, MOV):
+        super().__init__()
+        self.maxHP = maxHP
+        self.HP = self.maxHP
+        self.ATK = ATK
+        self.MOV = MOV
+        self.animCycle = 15
+
+class Cursor(GenericAnimatedSprite):
+
+    def __init__(self):
+        super().__init__()
+        self.images = load_images("cursor")
+        self.animCycle = 5
+
+class Vampire(PlayerUnitSprite):
+
+    def __init__(self):
+        PlayerUnitSprite.__init__(self,5,3,4)
+        self.images = load_images("vampire")
+
+class Werewolf(PlayerUnitSprite):
+
+    def __init__(self):
+        PlayerUnitSprite.__init__(self,5,8,2)
+        self.images = load_images("werewolf")
